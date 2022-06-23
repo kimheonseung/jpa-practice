f1462c77aba1128eeac2b84a5cb74300178d0068
kimheonseung <khs920210@gmail.com>
2022-06-23 23:23:45 +0900

    chapter5.3~

--- src/main/java/com/devh/example/jpa/chapter5/Chapter5Main.java
+++ src/main/java/com/devh/example/jpa/chapter5/Chapter5Main.java
@@ -1,5 +1,7 @@
 package com.devh.example.jpa.chapter5;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
@@ -21,6 +23,7 @@ public class Chapter5Main {
 			tx.begin();
 			
 			testSave(em);
+			deleteRelationEntity(em);
 			
 			tx.commit();
 		} catch (Exception e) {
@@ -46,5 +49,52 @@ public class Chapter5Main {
 		Member member2 = new Member("member2", "회원2");
 		member2.setTeam(team1); // 연관관계 설정 member2 -> team1
 		em.persist(member2);
+	}
+	
+	private static void testFind(EntityManager em) {
+		/*
+		 * 연관관계가 있는 엔티티를 조회하는 방법
+		 * 	- 1. 객체 그래프 탐색
+		 * 	- 2. 객체지향 쿼리 사용 (JPQL)
+		 */
+		
+		Member member1 = em.find(Member.class, "member1");
+		Team team = member1.getTeam();
+		System.out.println("teamId: " + team.getId() + ", teamName: " + team.getName());
+	}
+	
+	private static void testJPQL(EntityManager em) {
+		String jpql = "select m from Member m join m.team t where t.name=:teamName";
+		List<Member> resultList = em.createQuery(jpql, Member.class)
+				.setParameter("teamName", "팀1")
+				.getResultList();
+		
+		for(Member m : resultList) {
+			System.out.println("[query] member.username: " + m.getUsername());
+		}
+	}
+	
+	private static void updateRelation(EntityManager em) {
+		// 새로운 팀2
+		Team team2 = new Team("team2", "팀2");
+		em.persist(team2);
+		
+		// 회원1에 새로운 팀2 설정
+		Member member1 = em.find(Member.class, "member1");
+		member1.setTeam(team2);
+	}
+	
+	private static void deleteRelation(EntityManager em) {
+		Member member1 = em.find(Member.class, "member1");
+		member1.setTeam(null);
+	}
+	
+	private static void deleteRelationEntity(EntityManager em) {
+		Member member1 = em.find(Member.class, "member1");
+		Member member2 = em.find(Member.class, "member2");
+		member1.setTeam(null);
+		member2.setTeam(null);
+		Team team = em.find(Team.class, "team1");
+		em.remove(team);
 	}
 }
